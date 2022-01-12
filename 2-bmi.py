f3570e10872304c626b74b436343ff1a9c915b7f
yuuki_nyanko <yuukinyanko@gmail.com>
2022-01-12 23:00:41 +0900

    2日目

---
+++ 2-bmi.py
@@ -0,0 +1,4 @@
+h = float(input("身長は何cmですか？")) / 100.0
+w = float(input("体重は何kgですか？"))
+bmi = w / (h * h)
+print("あなたのBMIは",bmi,"です。")
---
+++ 4-turtle2.py
@@ -0,0 +1,7 @@
+from turtle import *
+shape("turtle")
+# pythonはスペースを使って繰り返し部分を表す
+for i in range(4):
+    forward(100)
+    left(90)
+done()
